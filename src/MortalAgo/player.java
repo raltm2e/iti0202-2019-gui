18077ea349ccc7524ed09267d2fa714b914e362c
roaltm <roaltm@ttu.ee>
2019-03-02 14:31:03 +0200

    test

---
+++ src/MortalAgo/player.java
@@ -0,0 +1,31 @@
+package MortalAgo;
+
+import javafx.application.Application;
+
+public abstract class player {
+    private int hp;
+    private int attack;
+    private int defence;
+
+    public void createPlayer(String name) {
+        if (name.equals("Ago")) {
+            player newPlayer = new ago();
+        }
+        this.attack = 1;
+        this.defence = 1;
+        this.hp = 10;
+    }
+
+    public int getAttack() {
+        return this.attack;
+    }
+
+    public int getDefence() {
+        return this.defence;
+    }
+
+    public int getHp() {
+        return this.hp;
+    }
+
+}
