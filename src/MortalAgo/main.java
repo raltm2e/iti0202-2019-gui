e2e60421d5c45b0f39426e96170cd9318eab737f
roaltm <roaltm@ttu.ee>
2019-04-20 21:20:14 +0300

    Added music files for win and lose

--- src/MortalAgo/main.java
+++ src/MortalAgo/main.java
@@ -31,7 +31,7 @@ public class main extends Application {
     private int startButtonY = 200, startButtonX = 530;
     private int statpoints = 4;
     private double volumeMultiplier = 1.0;
-    private MediaPlayer mediaPlayerMenu, mediaPlayerGame;
+    private MediaPlayer mediaPlayerMenu, mediaPlayerGame, mediaPlayerLose, mediaPlayerStats;
 
     private void startGame(Stage stage) {
         int roundcounter = 0;
@@ -50,15 +50,19 @@ public class main extends Application {
         stage.show();
         stage.setResizable(false);
 
-        List<MenuItem> menuItems = makeMenuButtons();
-        root.getChildren().addAll(menuItems);
-
         String musicFile = "src/MortalAgo/Media/K2h.mp3";
         Media sound = new Media(new File(musicFile).toURI().toString());
         mediaPlayerMenu = new MediaPlayer(sound);
         mediaPlayerMenu.setCycleCount(MediaPlayer.INDEFINITE);
         mediaPlayerMenu.setVolume(volumeMultiplier);
         mediaPlayerMenu.play();
+
+        configMenuButtons(root, stage, scene, img, roundcounter, agoAttack, agoMaxHp);
+    }
+
+    private void configMenuButtons(Group root, Stage stage, Scene scene, ImageView img, int roundcounter, int agoAttack, int agoMaxHp) {
+        List<MenuItem> menuItems = makeMenuButtons();
+        root.getChildren().addAll(menuItems);
 
         MenuItem play = menuItems.get(0);
         play.setOnMouseClicked(event -> {
@@ -70,13 +74,14 @@ public class main extends Application {
 
         MenuItem settings = menuItems.get(1);
         settings.setOnMouseClicked(event -> {
-            doSettingsAction(root, scene, stage);
-            System.out.println("Settings");
+            root.getChildren().removeAll(menuItems);
+            doSettingsAction(root, scene, stage, img, roundcounter, agoAttack, agoMaxHp);
         });
 
         MenuItem extras = menuItems.get(2);
         extras.setOnMouseClicked(event -> {
-            System.out.println("Extras");
+            root.getChildren().removeAll(menuItems);
+            doExtrasAction(root, scene, stage, img, roundcounter, agoAttack, agoMaxHp);
         });
 
         MenuItem exit = menuItems.get(3);
@@ -120,6 +125,30 @@ public class main extends Application {
         return menuItems;
     }
 
+    private void doExtrasAction(Group root, Scene scene, Stage stage, ImageView img, int roundcounter, int agoAttack, int agoMaxHp) {
+        Text title = new Text(startButtonX + 10, startButtonY - 20, "Credits");
+        title.setFont(new Font("Comic Sans MS", 30));
+        root.getChildren().add(title);
+
+        Text raul = new Text(startButtonX + 10, startButtonY + 30, "Raul Altmäe");
+        makeAuthorText(raul, root);
+
+        Text robert = new Text(startButtonX + 10, startButtonY + 80, "Robert Altmäe");
+        makeAuthorText(robert, root);
+
+        MenuItem menuButton = new MenuItem("Main menu");
+        menuButton.setTranslateX(startButtonX);
+        menuButton.setTranslateY(startButtonY + 150);
+        root.getChildren().add(menuButton);
+        menuButton.setOnMouseClicked(mouseEvent -> {
+            root.getChildren().remove(menuButton);
+            root.getChildren().remove(title);
+            root.getChildren().remove(raul);
+            root.getChildren().remove(robert);
+            configMenuButtons(root, stage, scene, img, roundcounter, agoAttack, agoMaxHp);
+        });
+    }
+
     private void doPlayAction(Group root, Scene scene, Stage stage, int roundcounter, int agoAttack, int agoMaxHp) {
         Canvas canvas = new Canvas(600, 600);
         Image background = new Image( "file:src\\MortalAgo\\Media\\Background\\IT_Kolledž.jpg" );
@@ -140,7 +169,6 @@ public class main extends Application {
             vastane = new Gert(enemy2, enemy2Logo, test, 10, 105);
         }
         test.drawEnemy(vastane, 400.0, 310.0);
-        System.out.println(test.getEnemy());
         test.drawAgo(ago, 100.0,310.0);
         test.drawHpRectangle(ago);
         test.drawHpRectangle(vastane);
@@ -169,24 +197,15 @@ public class main extends Application {
         }.start();
     }
 
-    private void doSettingsAction(Group root, Scene scene, Stage stage) {
-        root.getChildren().clear();
-        Text volumeText = new Text(startButtonX - 20, startButtonY, "Volume: " + (this.volumeMultiplier * 100));
-        volumeText.setFont(new Font("Comic Sans", 24));
+    private void doSettingsAction(Group root, Scene scene, Stage stage, ImageView img, int roundcounter, int agoAttack, int agoMaxHp) {
+        Text volumeText = new Text(startButtonX, startButtonY - 20, "Volume: " + Math.round(this.volumeMultiplier * 100));
+        volumeText.setFont(new Font("Comic Sans MS", 24));
         root.getChildren().add(volumeText);
-
-        MenuItem menuButton = new MenuItem("Main menu");
-        menuButton.setTranslateX(startButtonX);
-        menuButton.setTranslateY(startButtonY + 150);
-        root.getChildren().add(menuButton);
-        menuButton.setOnMouseClicked(mouseEvent -> {
-            restartGame(stage);
-        });
 
         Button volumeIncreaseButton = makeStatButton(startButtonX, startButtonY, "Increase");
         root.getChildren().add(volumeIncreaseButton);
         volumeIncreaseButton.setOnMouseClicked(mouseEvent -> {
-            if (volumeMultiplier >= 1) {
+            if (Math.round(this.volumeMultiplier * 100) >= 100) {
                 System.out.println("Volume is at max");
             } else {
                 this.volumeMultiplier += 0.1;
@@ -198,7 +217,7 @@ public class main extends Application {
         Button volumeDecreaseButton = makeStatButton(startButtonX, startButtonY + 50, "Decrease");
         root.getChildren().add(volumeDecreaseButton);
         volumeDecreaseButton.setOnMouseClicked(mouseEvent -> {
-            if (volumeMultiplier <= 0) {
+            if (Math.round(this.volumeMultiplier * 100) <= 0) {
                 System.out.println("Volume is at min");
             } else {
                 this.volumeMultiplier -= 0.1;
@@ -206,22 +225,41 @@ public class main extends Application {
                 volumeText.setText("Volume: " + Math.round((this.volumeMultiplier * 100)));
             }
         });
+
+        MenuItem menuButton = new MenuItem("Main menu");
+        menuButton.setTranslateX(startButtonX);
+        menuButton.setTranslateY(startButtonY + 150);
+        root.getChildren().add(menuButton);
+        menuButton.setOnMouseClicked(mouseEvent -> {
+            root.getChildren().remove(volumeIncreaseButton);
+            root.getChildren().remove(volumeDecreaseButton);
+            root.getChildren().remove(volumeText);
+            root.getChildren().remove(menuButton);
+            configMenuButtons(root, stage, scene, img, roundcounter, agoAttack, agoMaxHp);
+        });
     }
 
     private void makeStatsWindow(Group root, Scene scene, Stage stage, World world, int roundcounter) {
         world.getRoot().getChildren().clear();
         statpoints = 4;
 
+        String musicFile = "src/MortalAgo/Media/jan_uuspõld.mp3";
+        Media sound = new Media(new File(musicFile).toURI().toString());
+        mediaPlayerStats = new MediaPlayer(sound);
+        mediaPlayerStats.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayerStats.play();
+        mediaPlayerStats.setVolume(volumeMultiplier);
+
         Text stats = new Text(280, 180, "Statpoints: " + statpoints);
-        stats.setFont(new Font("Comic Sans", 30));
+        stats.setFont(new Font("Comic Sans MS", 30));
         root.getChildren().add(stats);
 
         Text Attack = new Text(280, 230, "Attack: " + world.getPlayer().getAttack());
-        Attack.setFont(new Font("Comic Sans", 24));
+        Attack.setFont(new Font("Comic Sans MS", 24));
         root.getChildren().add(Attack);
 
         Text hitPoints = new Text(280, 280, "Hitpoints: " + world.getPlayer().getMaxHp());
-        hitPoints.setFont(new Font("Comic Sans", 24));
+        hitPoints.setFont(new Font("Comic Sans MS", 24));
         root.getChildren().add(hitPoints);
 
         Button AttackButton = makeStatButton(450, 200, "+1 Attack");
@@ -251,6 +289,7 @@ public class main extends Application {
         continueGame.setTranslateY(400);
         root.getChildren().add(continueGame);
         continueGame.setOnMouseClicked(mouseEvent -> {
+            mediaPlayerStats.stop();
             makeNewWorld(root, scene, stage, world, roundcounter, world.getPlayer().getAttack(), world.getPlayer().getMaxHp());
         });
 
@@ -271,13 +310,20 @@ public class main extends Application {
     private void makeLoseWindow(Group root, Scene scene, Stage stage, World world) {
         world.getRoot().getChildren().clear();
 
+        String musicFile = "src/MortalAgo/Media/spanish_laugh.mp3";
+        Media sound = new Media(new File(musicFile).toURI().toString());
+        mediaPlayerLose = new MediaPlayer(sound);
+        mediaPlayerLose.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayerLose.play();
+        mediaPlayerLose.setVolume(volumeMultiplier);
+
         Text text = new Text("You died, git gud");
-        text.setFont(new Font("Comic Sans", 36));
+        text.setFont(new Font("Comic Sans MS", 36));
         text.setX(260);
         text.setY(100);
         root.getChildren().add(text);
 
-        MenuItem mainMenu = new MenuItem("Return to Main Menu");
+        MenuItem mainMenu = new MenuItem("Play again");
         mainMenu.setTranslateX(290);
         mainMenu.setTranslateY(150);
         root.getChildren().add(mainMenu);
@@ -306,11 +352,19 @@ public class main extends Application {
         AttackButton.setLayoutX(x);
         AttackButton.setLayoutY(y);
         AttackButton.setText(text);
-        AttackButton.setStyle("-fx-background-color: linear-gradient(#ff5400, #be1d00);\n" +
-                "    -fx-background-radius: 100;\n" +
-                "    -fx-background-insets: 0;\n" +
-                "    -fx-font-size: 18;\n" +
-                "    -fx-text-fill: white;");
+        AttackButton.setStyle("-fx-background-color: \n" +
+                "        #090a0c,\n" +
+                "        linear-gradient(#38424b 0%, #1f2429 20%, #191d22 100%),\n" +
+                "        linear-gradient(#20262b, #191d22),\n" +
+                "        radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0));\n" +
+                "    -fx-background-radius: 5,4,3,5;\n" +
+                "    -fx-background-insets: 0,1,2,0;\n" +
+                "    -fx-text-fill: white;\n" +
+                "    -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );\n" +
+                "    -fx-font-family: \"Comic Sans MS\";\n" +
+                "    -fx-text-fill: linear-gradient(white, #d0d0d0);\n" +
+                "    -fx-font-size: 18px;\n" +
+                "    -fx-padding: 10 20 10 20;");
         AttackButton.setOnMouseEntered(event -> {
             AttackButton.setOpacity(0.7);
         });
@@ -324,6 +378,14 @@ public class main extends Application {
             AttackButton.setOpacity(1);
         });
         return AttackButton;
+    }
+
+    private void makeAuthorText(Text text, Group root) {
+        text.setFont(new Font("Verdana", 26));
+        text.setFill(Color.BLACK);
+        text.setStrokeWidth(0.2);
+        text.setStroke(Color.WHITE);
+        root.getChildren().add(text);
     }
 
     private static class MenuItem extends StackPane {
@@ -341,7 +403,7 @@ public class main extends Application {
 
             Text text = new Text(name);
             text.setFill(Color.DARKGREY);
-            text.setFont(Font.font("Comic Sans", FontWeight.SEMI_BOLD,20));
+            text.setFont(Font.font("Comic Sans MS", FontWeight.SEMI_BOLD,20));
 
             setAlignment(Pos.CENTER);
             getChildren().addAll(bg, text);
